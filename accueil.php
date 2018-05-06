0e1ecba9db2317fd70484030cdf3ca92c187b6be
dcognet <36398384+dcognet@users.noreply.github.com>
2018-05-06 12:00:46 +0100

    début css sur top tendances lien

--- accueil.php
+++ accueil.php
@@ -42,7 +42,7 @@ if (isset($_GET['link'],$_GET['commentaire'])){
               <form class="form_share" action="accueil.php"  method="GET" >
                 <input  class="input_share form-control" type="url" name="link" placeholder="Partager un lien">
                  <input style="padding-bottom:30px"class="input_share form-control" type="text" name="commentaire" placeholder="Commenter le lien" required>
-                 <input style="width:auto;" type="submit" value="Partager" class=" btn btn-primary pull-right"required>
+                 <input style="width:auto;" type="submit" value="Partager" class="btn btn-primary pull-right"required>
               </form>
             </div>
         </div>
@@ -51,34 +51,57 @@ if (isset($_GET['link'],$_GET['commentaire'])){
 
 
     <!-- Partie pour visualisé les 5 articles ayant eux le plus d'intéractions dans la journée-->
-    <section style="border :solid; margin:10px; padding:15px;">
-      <h3>Liste des 5 liens ayant eux le plus d'intéractions dans la journée</h3>
-      <?php
-      interaction_number_update();
-      $liste_lien ="SELECT * FROM links ORDER BY interaction_number DESC LIMIT 5";
-      $articles = selectionner_id_link($liste_lien);
-      foreach ($articles as $article){
-        ?>
-        <div class="row" style="border-top : solid;">
-          <article>
-            <a class="nav-link active" href="<?=$article['link']?>"><?="Lien = ".$article['link']?></a><br/>
-            <span><?= "Date = ".$article['date']?></span><br/>
-            <span><?="Utilisateur = ".$article['id_user']?></span><br/>
-            <span><?= "Commentaire de l'utilisateur = ".$article['comment_user']?></span><br/>
+    <div class="row" style="padding-top: 10px">
+      <div class="col-md-6" style="margin:auto;">
+        <div class="card" style="width: auto; " >
+            <div class="card-title">
+                <h3 style="text-align: center">Liste des 5 liens ayant eux le plus d'intéractions dans la journée</h3>
+             </div>
+             <?php
+             interaction_number_update();
+             $liste_lien ="SELECT * FROM links ORDER BY interaction_number DESC LIMIT 5";
+             $articles = selectionner_id_link($liste_lien);
+             foreach ($articles as $article){
+               $pseudo=pseudo_de_user($article['id_user']);
+               ?>
+               <div class="container-fluid"style="padding-bottom:10px" >
+                 <div class="card" style="width: auto;">
+         					<div>
+         						<p class="titre_lien pseudo"><?="".$pseudo?></p>
 
-            <!-- Zone php à supprimer -->
-            <?php
-            $nombre_interaction = compteur_interaction($article['id_link']);
-            echo "Il y a eu ".$nombre_interaction." intéractions sur cet article.</br>";
-            ?>
+         						<p class="titre_lien date"><?= "".$article['date']?></p>
+         					</div>
+                  <p class="card-text">
+                    <?= "".$article['comment_user']?>
+                  </p>
+         		  		<p class="card-text lien">
+                    <a class="nav-link active" href="<?=$article['link']?>"><?="Lien = ".$article['link']?></a><br/>
+                  </p>
+         				  <div class="card-footer">
+         				  	<img src="https://www.stickers-shopping.fr/prestashop/img/p/1564-1626-large.jpg" style="width:20px;height:20px;">
+         				    <a href="">Upvote</a>
 
-            <a href="pagelien.php?id_link=<?=$article['id_link']?>">Ouvrir page du lien</a>
-          </arcticle>
+                    <?php
+                    echo compteur_vote('links',$article['id_link'],'positif');
+                     ?>
+         				    <img src="http://www.stickers-shopping.fr/prestashop/img/p/1567-1629-large.jpg" style="width:20px;height:20px;">
+         				    <a href="">Downvote</a>
+                    <?php echo compteur_vote('links',$article['id_link'],'négatif') ?>
+                    <a href="pagelien.php?id_link=<?=$article['id_link']?>">Ouvrir page du lien</a>
+
+         				  </div>
+         				</div>
+              </div>
+
+               <?php
+             }
+             ?>
+
         </div>
-        <?php
-      }
-      ?>
-    </section>
+      </div>
+    </div>
+
+
 
 
     <!-- Partie pour visualisé tous les articles postés il y a 24 heures -->
